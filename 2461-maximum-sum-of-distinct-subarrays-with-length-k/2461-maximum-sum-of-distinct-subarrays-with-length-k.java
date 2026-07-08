4f9d0bd3afa381572e6e4f6a8aba36a4530370db
Parneet Kaur <pk7211171@gmail.com>
2026-07-08 18:33:39 +0530

    Time: 55 ms (51.03%), Space: 99.5 MB (14.96%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -9,11 +9,12 @@ class Solution {
             map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
 
             if(r - l + 1 > k){
-                sum -= nums[l];
                 map.put(nums[l], map.get(nums[l]) - 1);
                 if(map.get(nums[l]) == 0){
                     map.remove(nums[l]);
                 }
+                sum -= nums[l];
+                
                 l++;
             }
             if(r - l + 1 == k && map.size() == k){
