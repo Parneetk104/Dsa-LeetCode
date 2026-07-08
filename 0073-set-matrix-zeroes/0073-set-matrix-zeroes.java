57e836bb822e174d3f7f147b77cdc5b418fc725e
Parneet Kaur <pk7211171@gmail.com>
2026-07-08 18:56:08 +0530

    Time: 1 ms (98.55%), Space: 47.6 MB (43.42%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,23 +2,28 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        boolean[] row = new boolean[n];
-        boolean[] col = new boolean[m];
-       
+        boolean firstColZero = false;
+       //first pass :  Stored Markers --------->>>>
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+            }
+            for(int j = 1; j < m; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = true;
-                    col[j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(row[i] || col[j]){
+        //2nd pass : fill from bottom right
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 1; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
-                    
                 }
+            }
+            if(firstColZero){
+                matrix[i][0] = 0;
             }
         }
     }
