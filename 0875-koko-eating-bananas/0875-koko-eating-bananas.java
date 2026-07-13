c8e0b22b6dda8fbfc897a9c6b0a7bd8dc2475b00
Parneet Kaur <pk7211171@gmail.com>
2026-07-13 11:58:37 +0530

    Time: 26 ms (16.52%), Space: 48.4 MB (16.45%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,44 @@
+class Solution {
+    private Boolean canFinish(int[] piles, int h, int speed){
+        int totalHours = 0;
+        for(int pile: piles){
+            totalHours += (Math.ceil((double)pile / speed));
+        }
+        if(totalHours <= h){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        // //min speed = 1, max = max(piles)/h
+        int low = 1, high;
+        int max = -1;
+        for(int i = 0; i < piles.length; i++){
+            max = Math.max(max, piles[i]);
+        }
+        high = max;
+        int ans = Integer.MAX_VALUE;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(canFinish(piles, h, mid)){
+                ans = mid;
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+            
+        }
+        return ans;
+       
+
+    }
+}
+
+/*
+Once a speed becomes valid, every larger speed is also valid.
+This is called a monotonic property.
+as after k = 4, 5, 6, everything is vallid"
+*/
