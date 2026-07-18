6549571eb4ed126a1099ea8bf5fed0f1af353505
Parneet Kaur <pk7211171@gmail.com>
2026-07-18 12:04:51 +0530

    Time: 1 ms (100%), Space: 48.5 MB (94.37%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,25 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length, n2 = nums2.length;
-        int[] arr = new int[n1 + n2];
-        int i = 0, j = 0, k = 0;
+        int i = 0, j = 0;
+        int n = n1 + n2;
+        int idx2 = n/2, idx1 = idx2 - 1;
+        int count = 0;
+        int idx1el = -1, idx2el = -1;
         while(i < n1 && j < n2){
             if(nums1[i] < nums2[j]){
-                arr[k++] = nums1[i++];
-            }else {
-                arr[k++] = nums2[j++];
+                if(count == idx1) idx1el = nums1[i];
+                if(count == idx2) idx2el = nums1[i];
+                count++;
+                i++;
+            }
+            else {
+                if(count == idx1) idx1el = nums2[j];
+                if(count == idx2) idx2el = nums2[j];
+                count++;
+                j++;
             }
         }
         while(i < n1){
-            arr[k++] = nums1[i++];
+            if(count == idx1) idx1el = nums1[i];
+            if(count == idx2) idx2el = nums1[i];
+                count++;
+                i++;
         }
         while(j < n2){
-            arr[k++] = nums2[j++];
+            if(count == idx1) idx1el = nums2[j];
+            if(count == idx2) idx2el = nums2[j];
+                count++;
+                j++;
         }
-        int n = n1 + n2;
-        if(n % 2 == 1){
-            return arr[n/2];
-        }
-        return (double)((double)(arr[n/2 - 1]) + (double)(arr[n/2])) / 2.0;
+        if(n % 2 == 1) return idx2el;
+        return (double)(idx1el + idx2el)/2;
     }
 }
