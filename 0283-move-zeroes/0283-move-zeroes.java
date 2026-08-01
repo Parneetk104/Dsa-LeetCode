5054f8bd6f4cfc474478945d8cbdb4720db0f8c5
Parneet Kaur <pk7211171@gmail.com>
2026-08-01 11:46:06 +0530

    Time: 2 ms (92.12%), Space: 48 MB (20.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int slow = 0;
+        int slow = 0;
+        for(int fast = 0; fast < nums.length; fast++){
+            if(nums[fast] != 0){
+                int temp = nums[fast];
+                nums[fast] = nums[slow];
+                nums[slow]  = temp;
+                slow++;
+            }
+        }
+    }
+}
+
+
+/*
+ int slow = 0;
       //scanner
       for(int fast = 0; fast < nums.length; fast++){
         if(nums[fast] != 0){
@@ -11,8 +25,4 @@ class Solution {
             
         }
          
-      }
-      
-      
-    }
-}
+      }*/
