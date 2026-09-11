9368bbde8d0496edfb781edd98b89f2840d2c304
Parneet Kaur <pk7211171@gmail.com>
2026-09-11 12:11:06 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,20 @@
 class Solution {
+    static int[] dp;
     public int rob(int[] nums) {
-        int prev = 0;          //best till prev  if skip
-        int prevprev = 0;     // best till prevprev if not skip
-        for(int money: nums){
-            int take = money + prevprev;
-            int skip = prev;
-            int curr = Math.max(take, skip);
-            prevprev = prev;
-            prev = curr;
-        }
+        dp = new int[nums.length];    // 0 to n - 1
+        Arrays.fill(dp, -1); // mark krna 
+        return loot(0, nums);
+    }
 
-        return prev;
-        
+    public int loot(int i, int[] nums){
+        //BASE CASE
+        if(i >= nums.length) return 0;     
+        //if had already stored ans
+        if(dp[i] != -1)  return dp[i];
+        int pick = nums[i] + loot(i + 2, nums);    //believe in recursion that we will get max, write chocies
+        int skip = loot(i + 1, nums);
+        int ans = Math.max(pick, skip);
+        dp[i] = ans;
+        return ans;
     }
 }
