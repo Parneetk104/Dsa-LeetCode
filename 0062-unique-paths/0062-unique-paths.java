b93c7383264027384dc6aab87f0f174e442e0a6e
Parneet Kaur <pk7211171@gmail.com>
2026-09-23 14:09:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.68%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n]; 
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+
+        return count(m - 1, n - 1, m, n, dp);
+        
+    }
+
+    public int count(int i, int j, int m, int n, int[][] dp){
+      
+        if(i ==0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = count(i - 1, j, m, n, dp);
+        int left = count(i, j - 1, m, n, dp);
+        dp[i][j] = up + left;
+        return dp[i][j];
+    }
+}
